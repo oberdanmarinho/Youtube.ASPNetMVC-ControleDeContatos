225387aa590a3a15ee42e20b3b8a22fd81850c08
Oberdan Marinho <oberdanmarinho@gmail.com>
2023-07-10 23:53:04 -0300

    Criação Model

---
+++ ControleDeContatos/ControleDeContatos/Data/BancoContext.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ControleDeContatos.Data
+{
+  public class BancoContext
+  {
+
+  }
+}
