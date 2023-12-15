03cf440057fa6f42d5d597c73295cabdfa319736
Oberdan Marinho <oberdanmarinho@gmail.com>
2023-12-15 16:10:59 -0300

    Criando Migration

--- ControleDeContatos/ControleDeContatos/Startup.cs
+++ ControleDeContatos/ControleDeContatos/Startup.cs
@@ -5,10 +5,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+
 
 namespace ControleDeContatos
 {
